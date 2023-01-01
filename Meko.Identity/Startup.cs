b4782ca6e99c287514e8e0a74e7e792fae92af00
Mahmoud <mahmoud@grabbd.com>
2023-01-02 00:34:39 +0200

    Add startup and Csharpier first formate

--- Meko.Identity/Startup.cs
+++ Meko.Identity/Startup.cs
@@ -1,6 +1,35 @@
-﻿namespace Meko.Identity
+﻿namespace Meko.Identity;
+
+public class Startup
 {
-    public class Startup
+    public IConfiguration configRoot { get; }
+
+    public Startup(IConfiguration configuration)
     {
+        configRoot = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(WebApplication app, IWebHostEnvironment env)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
